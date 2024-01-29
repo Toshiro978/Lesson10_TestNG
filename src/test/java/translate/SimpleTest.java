3043acad32c74734d963d970e11f1fe1f7bc9e1b
s.chugaevsky <s.chugaevsky@gmail.com>
2024-01-30 01:51:44 +0200

    Google translate test

--- src/test/java/translate/SimpleTest.java
+++ src/test/java/translate/SimpleTest.java
@@ -1,22 +1,50 @@
 package translate;
+import static com.codeborne.selenide.Selenide.*;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
+import com.codeborne.selenide.Condition;
+import config.Browser;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-
 public class SimpleTest {
-
-    @Test
-    public void myFirstTest(){
-        System.out.println("Hello world!");
+    @DataProvider(name = "languages")
+    public Object[][] languages() {
+        return new Object[][] {
+                {"en", "I'll study TESTNG cool."},
+                {"es", "Estudiaré testng genial."},
+                {"da", "Jeg studerer testng cool."},
+                {"it", "Studierò Testng Cool."},
+                {"de", "Ich werde testng cool studieren."},
+                {"bg", "Ще проуча Testng Cool."},
+                {"bo", "I'll study TESTNG cool."},
+                {"ca", "Estudiaré Testng Cool."},
+                {"cl", "I'll study TESTNG cool."},
+                {"dz", "I'll study TESTNG cool."},
+                {"ee", "Masrɔ̃ nu tso testng cool ŋu."},
+                {"fr", "Je vais étudier le test."},
+                {"ge", "I'll study TESTNG cool."},
+                {"hu", "Tanulok a tesztng hűvösen."},
+                {"ie", "I'll study TESTNG cool."},
+                {"lv", "Es studēšu testng atdzist."},
+                {"pl", "Badam testng fajnie."},
+                {"ro", "Voi studia testng cool."},
+                {"sk", "Budem študovať Testng Cool."},
+                {"sv", "Jag studerar testng cool."},
+        };
     }
-
-    @Test
-    public void top250MoviesPageTitle() {
-        open("https://www.imdb.com/chart/top/");
-        $("h1.ipc-title__text").shouldHave(text("IMDb Top 250 Movies"));
+    @BeforeTest
+    public void setUpBrowser() {
+        Browser.setBrowser();
+    }
+    @Test(dataProvider = "languages")
+    public void myFirstTest(String baseUrl, String response){
+        String dynamicURL = "https://translate.google.com/?sl=uk&tl=" + baseUrl + "&text=Я%20круто%20вивчу%20TestNG.&op=translate";
+        open(dynamicURL);
+        $("[jsname='W297wb']").should(Condition.visible);
+        String text = $("[jsname='W297wb']").getAttribute("textContent");
+        Assert.assertEquals(text, response);
+        System.out.println("Test passed - " + baseUrl);
     }
 }
